06dd746468ee593697fda4dd5cb1997864e227fa
Mohamed_Mugrabhi <mohamed.mamdouh.mugrabhi@gmail.com>
2026-01-15 01:32:17 +0200

    Update Validation.java

--- src/util/Validation.java
+++ src/util/Validation.java
@@ -5,6 +5,7 @@ public class Validation {
     public static boolean isEmpty(String text){
         return text == null || text.trim().isEmpty();
     }
+// trigger linguist
 
     public static boolean isValidPhone(String phone){
         return phone.matches("\\d{11}");
